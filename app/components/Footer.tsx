b11fc403939e4b468f239ad91f26a55cdc8811dd
Tej Lehal <tejlehal13@gmail.com>
2024-10-22 15:52:26 -0400

    added CommonWrapper in the setup

---
+++ app/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+    <footer className="flex justify-center items-center p-4 bg-background text-foreground">
+        <p>All rigths reserved @ 2024</p>
+    </footer>
+    );
+  }
+  
---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+    <header className="flex justify-between items-center p-4 bg-background text-foreground">
+      <nav></nav>
+      
+    </header>
+    );
+  }
+  
