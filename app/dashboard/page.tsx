a26d027b41646c92732aaf4fe58c3dfa63b6459a
Tej Lehal <tejlehal13@gmail.com>
2024-10-23 17:07:07 -0400

    added login page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+// import Image from "next/image";
+
+import CommonWrapper from "../components/CommonWrapper";
+
+export default function Dashboard() {
+  return (
+    <CommonWrapper>
+      <div className="isolate px-6 pt-14 lg:px-8">
+        <h1 className="text-4xl text-center mx-auto font-bold text-foreground">Welcome to The Titos Dashboard!</h1>
+      </div>
+      {/* <Image
+        src="/logo.svg"
+        alt="Logo"
+        width={144}
+        height={144}
+      /> */}
+    </CommonWrapper>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,49 @@
-// import Image from "next/image";
+'use client'
+import Image from "next/image";
+import { useState } from "react";
 
 import CommonWrapper from "./components/CommonWrapper";
+import Dashboard from "./dashboard/page";
 
 export default function Home() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.values);
+    // Check if credentials are valid
+    setLoggedIn(!loggedIn); // Replace with actual login logic);
+  };
   return (
     <CommonWrapper>
-      <div className="isolate px-6 pt-14 lg:px-8">
-        <h1 className="text-4xl text-center mx-auto font-bold text-foreground">Welcome to The Titos Dashboard!</h1>
-      </div>
-      {/* <Image
-        src="/logo.svg"
-        alt="Logo"
-        width={144}
-        height={144}
-      /> */}
+      <section className="flex justify-center items-center h-screen">
+        <div className="log-in-section">
+          <div className="log-in-image">
+            <Image
+              src="/images/titos.png"
+              alt="Logo"
+              width={150}
+              height={150}
+            />
+          </div>
+          {loggedIn ? <Dashboard/> : (
+            <div className="log-in">
+              <h2>Log In</h2>
+              <form onSubmit={(e) => handleSubmit(e)}>
+                <label>
+                  Username
+                  <input type="text" name="username" required/>
+                </label>
+                <label>
+                  Password
+                  <input type="password" name="password" required/>
+                </label>
+                <input type="submit" value="Submit" />
+              </form>
+            </div>
+          )}
+        </div>
+      </section>
     </CommonWrapper>
   );
 }
